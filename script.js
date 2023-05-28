dd7597babca934e70aa850c7f65e75dd1dfef260
etiry <7997357+etiry@users.noreply.github.com>
2023-05-28 12:29:51 -0400

    Add initial files and functions

---
+++ script.js
@@ -0,0 +1,12 @@
+const gameBoard = (() => {
+	const generate = () => Array(9).fill('X');
+	return { generate };
+})();
+
+const displayController = (() => {
+
+})();
+
+const player = (marker) => {
+	return { marker };
+};
